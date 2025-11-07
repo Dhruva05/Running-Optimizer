faee65226e1757c7fc64033a2bf8daf692a2f89b
Dhruva <dshekdar@uwaterloo.ca>
2025-11-07 13:10:48 -0500

    Day 1: File Tree Updates

---
+++ scripts/ingest_local_csv.py
@@ -0,0 +1,7 @@
+import pathlib
+import pandas as pd
+
+RAW = pathlib.Path("/Users/dhruvashekdar/Running-Optimizer/activities.csv")
+OUT = Path("/Users/dhruvashekdar/Running-Optimizer/runs_clean.csv")
+
+
